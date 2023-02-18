69eb7da9b74b00918a97edc1c7c9c46d86621494
Cardkung <dark_card_game@hotmail.com>
2023-02-18 20:37:18 +0700

    itin

--- README.md
+++ README.md
@@ -21,3 +21,18 @@ if dataset_name == "Death from road accident":
         ax=ax,
     )
     st.pyplot(fig)
+
+
+df2["Date"]= pd.to_datetime(df2["Date"], infer_datetime_format=True)
+
+df2['Year'] = df2['Date'].dt.year
+df2['Month'] = df2['Date'].dt.month
+df2['Day'] = df2['Date'].dt.day
+df2['Quarter'] = df2['Date'].dt.quarter
+
+
+fig1 = plt.figure(figsize=(15,1))
+sns.lineplot(x=df2["Date"], y=df2["Total"], data=df2)
+plt.title("Total Death")
+
+st.pyplot(fig1)
--- main.py
+++ main.py
@@ -5,9 +5,8 @@ import numpy as np
 import seaborn as sns
 import plotly.express as px
 from datetime import datetime
-import warnings
-warnings.filterwarnings("ignore")
-
+import xgboost as xgb
+from sklearn.metrics import mean_squared_error
 
 
 st.set_page_config(page_title="AI dataset Dashboard",
@@ -22,12 +21,13 @@ st.markdown("## Select the dataset")
 
 df2 = pd.read_csv("./dataset/DataSet_Time.csv")
 st.write(df2.shape)
+
 df2["Date"]= pd.to_datetime(df2["Date"], infer_datetime_format=True)
 
-df2['Year'] = df2['Date'].dt.year
-df2['Month'] = df2['Date'].dt.month
-df2['Day'] = df2['Date'].dt.day
-df2['Quarter'] = df2['Date'].dt.quarter
+#df2['Year'] = df2['Date'].dt.year
+#df2['Month'] = df2['Date'].dt.month
+#df2['Day'] = df2['Date'].dt.day
+#df2['Quarter'] = df2['Date'].dt.quarter
 
 
 fig1 = plt.figure(figsize=(15,1))
@@ -36,28 +36,114 @@ plt.title("Total Death")
 
 st.pyplot(fig1)
 
+df2 = df2.set_index('Date')
+df2.index =  pd.to_datetime(df2.index)
+
+#st.write(df2)
+
+#------------------------------------------------------
+#Train / Split
+
+train = df2.loc[df2.index < '01-01-2019']
+test = df2.loc[df2.index >= '01-01-2019']
+
+figTrain = plt.figure(figsize=(15,1))
+sns.lineplot(x=train.index, y=train["Total"], data=train, color='orange',)
+plt.title("Train Set")
+st.pyplot(figTrain)
+
+figTest = plt.figure(figsize=(15,1))
+sns.lineplot(x=test.index, y=test["Total"], data=test, color='green',)
+plt.title("Test Set")
+st.pyplot(figTest)
+
+def create_features(df2):
+        """
+        Create Time series features based on time series index
+        """
+        df2['hour'] = df2.index.hour
+        df2['dayofweek'] = df2.index.day_of_week 
+        df2['quarter'] = df2.index.quarter
+        df2['month'] = df2.index.month
+        df2['year'] = df2.index.year
+        df2['dayofyear'] = df2.index.day_of_year
+        return df2
+
+df2 = create_features(df2)
+
+#Vistualize Features / Target Relationship
+
+fig2 = plt.figure(figsize=(10,5))
+sns.boxplot(data=df2, x='month', y='Total',)
+plt.title('Dead')
+#st.pyplot(fig2)
+
+#------------------------------------------------------
+# Create Model
+train = create_features(train)
+test = create_features(test)
+
+FEATURES = ['hour', 'dayofweek', 'quarter', 'month', 'year', 'dayofyear']
+TARGET = 'Total'
+
+X_train = train[FEATURES]
+y_train = train[TARGET]
+
+X_test = test[FEATURES]
+y_test = test[TARGET]
+
+reg = xgb.XGBRegressor(n_estimators=1000, early_stopping_round=50,
+                        learning_rate=0.001)
+reg.fit(X_train, y_train,
+        eval_set=[(X_train, y_train), (X_test, y_test)],
+        verbose=100
+)
+
+# Feature Importance
+fi = pd.DataFrame(data=reg.feature_importances_,
+                index=reg.feature_names_in_,
+                columns=['importance'])
+
+fi.sort_values('importance')
+#st.write(fi)
+
+#Forecast on Test
+
+test['prediction'] = reg.predict(X_test)
+df2 = df2.merge(test[['prediction']], how='left', left_index=True, right_index=True)
 
 st.write(df2)
+
+# TEST
+
+score = np.sqrt(mean_squared_error(test['Total'], test['prediction']))
+st.markdown(f'#### Mean Squared Error : {score:0.2f}')
+
+# Calculate Error
+
+test['error'] = np.abs(test[TARGET] - test['prediction'])
+test['date'] = test.index.date
+chk = test.groupby('date')['error'].mean().sort_values(ascending=False).head(5)
+chk2 = test.groupby('date')['error'].mean().sort_values(ascending=False).tail(5)
+
+l_column, r_column = st.columns(2)
+l_column.write("Top 5 Max Error")
+l_column.write(chk)
+r_column.write("Top 5 Max Correct")
+r_column.write(chk2)
+
 
 #------------------------------------------------------
 
 
-
-dataset_name = st.selectbox("Select Dataset", ("Death from road accident",))
-
-if dataset_name == "Death from road accident":
-        df = pd.read_csv("./dataset/DataSet_clean_CSV.csv",
-                         skiprows=0, #use when have blank row before data table
-                         #nrows=10000     #use when want to limit dataset  
+df = pd.read_csv("./dataset/DataSet_clean_CSV.csv",
+        skiprows=0, #use when have blank row before data table
+        #nrows=10000     #use when want to limit dataset  
         ) # read a CSV file inside the 'data" folder next to 'main.py'
         # df = pd.read_excel(...)  # will work for Excel files
         
-elif dataset_name == "Electric use in BKK":
-        df = pd.read_csv("./dataset/Electric use in BKK.csv") # read a CSV file inside the 'data" folder next to 'main.py'
-else:
-        None
 
-st.markdown(dataset_name)  # add a title
+st.markdown("# Dataset Dashboard")  # add a title
 #st.dataframe(df) #visualize my dataframe in the Streamlit app
 
 st.sidebar.header("Please filter here")
