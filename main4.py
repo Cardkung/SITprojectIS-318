339d1123f9256b2bd0f0cbfab552f8b224066058
Cardkung <dark_card_game@hotmail.com>
2023-03-03 16:26:42 +0700

    3MAR23

--- main4.py
+++ main4.py
@@ -8,6 +8,8 @@ import datetime
 import xgboost as xgb
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.model_selection import TimeSeriesSplit
+from sklearn.model_selection import RandomizedSearchCV
+from scipy.stats import randint, uniform
 
 
 st.set_page_config(page_title="AI dataset Dashboard",
@@ -20,7 +22,6 @@ st.markdown("# SIT Independent Study : Artificial intelligence (AI) prediction 6
 
 df2 = pd.read_csv("./dataset/DataSet_Time.csv")
 dfr = pd.read_csv("./dataset/data_time_r.csv")
-#st.write(dfr.shape)
 
 dfr["Date"]= pd.to_datetime(dfr["Date"], infer_datetime_format=True)
 
@@ -29,12 +30,6 @@ fig1 = plt.figure(figsize=(15,1))
 sns.lineplot(x=dfr["Date"], y=dfr["Total"], data=dfr)
 plt.title("Total Death")
 
-#figp1 = plt.figure(figsize=(15,1))
-#sns.lineplot(x=dfp["Date"], y=dfp["Total"], data=dfp)
-#plt.title("Total Death by Province")
-#st.pyplot(figp1)
-
-#st.pyplot(fig1)
 
 #Set Index
 dfr = dfr.set_index('Date')
@@ -62,7 +57,6 @@ plt.title("Train / Test DataSet")
 st.pyplot(figTrainTest)
 
 
-
 def create_features(dfr):
         """
         Create Time series features based on time series index
@@ -75,13 +69,6 @@ def create_features(dfr):
         return dfr
 
 dfr = create_features(dfr)
-
-#Vistualize Features / Target Relationship
-
-fig2 = plt.figure(figsize=(10,3))
-sns.boxplot(data=dfr, x='month', y='Total',)
-plt.title('Dead')
-#st.pyplot(fig2)
 
 #------------------------------------------------------
 # Create Model
@@ -98,26 +85,41 @@ X_test = test[FEATURES]
 y_test = test[TARGET]
 
 
-#Select N_Estimators
-n_est = st.select_slider('Select N_Estimators parameter', options=[300, 500, 800, 1000, 1500, 2000, 2500, 3000])
-st.write('Your N_Estimators is', n_est)
-
-#Select train time
-lrn_rate_pre = st.slider('Select Learning rate (milliseconds)', 1, 30, 5)
-lrn_rate = lrn_rate_pre/1000
-st.write('Your Learning rate is', lrn_rate, 'second')
+# Define the parameter distributions for the hyperparameters to tune
+param_dist  = {
+        "n_estimators": randint(low=300, high=2000),
+        "learning_rate": uniform(0.01, 0.1),
+}
 
 
-st.write("training....")
 
-reg = xgb.XGBRegressor(n_estimators=n_est, early_stopping_round=50,
-                        learning_rate=lrn_rate)
+# Train an XGBoost regression model using RandomizedSearchCV to tune the hyperparameters
+reg = xgb.XGBRegressor(objective="reg:squarederror")
+random_search = RandomizedSearchCV(
+estimator=reg,
+param_distributions=param_dist,
+cv=5,
+n_iter=10,
+n_jobs=-1,
+verbose=1,
+)
+random_search.fit(X_train, y_train)
+
+# Get the best hyperparameters and use them to train the final model
+best_params = random_search.best_params_
+
+
+st.write("Best parameters:", best_params)
+
+#st.write("training....")
+
+reg = xgb.XGBRegressor(**best_params)
 reg.fit(X_train, y_train,
         eval_set=[(X_train, y_train), (X_test, y_test)],
         verbose=100
 )
 
-st.markdown('#### :green[Complete]')
+#st.markdown('#### :green[Complete]')
 
 
 # Feature Importance
@@ -183,7 +185,7 @@ st.markdown(f'##### Prediction Chart on Time')
 dead_by_year = (
         future_df3['prediction']
 )
- 
+
 time_chart_line = pd.DataFrame(
         dead_by_year,
 )
@@ -224,14 +226,14 @@ right_column.plotly_chart(fig_dead_month, use_container_width=True)
 
 
 
+#'North', 'NorthEast', 'Central', 'East', 'West', 'South'
+r_show = 'North'
+r_show2 = 'NorthEast'
+r_show3 = 'Central'
+r_show4 = 'East'
+r_show5 = 'West'
+r_show6 = 'South'
 
-r_show = st.selectbox(
-        "Select Region to show:", ('North', 'NorthEast', 'Central', 'East', 'West', 'South')
-)
-
-#st.write(dfr)
-#st.write(future_df3)
-#st.write(r_show)
 
 futurepred = pd.date_range('2022-10-01', endpre, freq='1d')
 future_dfr = pd.DataFrame(index=futurepred,)
@@ -247,45 +249,200 @@ X_testr = test[FEATURES]
 y_testr = test[TARGETR]
 
 
-st.write("training....")
 
-reg2 = xgb.XGBRegressor(n_estimators=n_est, early_stopping_round=50,
-                        learning_rate=lrn_rate)
+reg2 = xgb.XGBRegressor(**best_params)
 
 reg2.fit(X_trainr, y_trainr,
         eval_set=[(X_trainr, y_trainr), (X_testr, y_testr)],
         verbose=100
 )
 
-st.markdown(f'#### Prediction Death of Region : :red[{r_show}]')
-
 future_dfr['prediction'] = reg2.predict(future_dfr[FEATURES]).round()
 
-#Write Table
-#st.write(future_dfr)
-
-
-#plot chart death region
-
-figpredr = plt.figure(figsize=(15,1))
-sns.lineplot(x=train.index, y=train[r_show], data=train, color='green', legend='auto', label="Train")
-sns.lineplot(x=test.index, y=test[r_show], data=test, color='orange', legend='auto', label="Test")
-sns.lineplot(x=future_dfr.index, y=future_dfr["prediction"], data=future_dfr, color='grey', legend='auto', label="Forecast")
-plt.title('Train / Test / Forecast DataSet of Region')
-st.pyplot(figpredr)
 
 # Make chart 3 timeline predict death by region
 
 dead_by_year_region = (
         future_dfr['prediction']
 )
- 
+
 time_chart_line_r = pd.DataFrame(
         dead_by_year_region,
 )
 
-# Create an area chart with a gradient of colors
-colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
 
-st.markdown(f'##### Prediction of Death on Time Region : {r_show}')
+#NorthEast
+TARGETR = r_show2
+
+X_trainr = train[FEATURES]
+y_trainr = train[TARGETR]
+
+X_testr = test[FEATURES]
+y_testr = test[TARGETR]
+
+
+reg2 = xgb.XGBRegressor(**best_params)
+
+reg2.fit(X_trainr, y_trainr,
+        eval_set=[(X_trainr, y_trainr), (X_testr, y_testr)],
+        verbose=100
+)
+
+future_dfr['prediction2'] = reg2.predict(future_dfr[FEATURES]).round()
+
+# Make chart 3 timeline predict death by region NE
+
+dead_by_year_region2 = (
+        future_dfr['prediction2']
+)
+
+time_chart_line_r2 = pd.DataFrame(
+        dead_by_year_region2,
+)
+
+
+#Central
+TARGETR = r_show3
+
+X_trainr = train[FEATURES]
+y_trainr = train[TARGETR]
+
+X_testr = test[FEATURES]
+y_testr = test[TARGETR]
+
+
+reg2 = xgb.XGBRegressor(**best_params)
+
+reg2.fit(X_trainr, y_trainr,
+        eval_set=[(X_trainr, y_trainr), (X_testr, y_testr)],
+        verbose=100
+)
+
+future_dfr['prediction3'] = reg2.predict(future_dfr[FEATURES]).round()
+
+# Make chart 3 timeline predict death by region central
+
+dead_by_year_region3 = (
+        future_dfr['prediction3']
+)
+
+time_chart_line_r3 = pd.DataFrame(
+        dead_by_year_region3,
+)
+
+
+#East
+TARGETR = r_show4
+
+X_trainr = train[FEATURES]
+y_trainr = train[TARGETR]
+
+X_testr = test[FEATURES]
+y_testr = test[TARGETR]
+
+
+reg2 = xgb.XGBRegressor(**best_params)
+
+reg2.fit(X_trainr, y_trainr,
+        eval_set=[(X_trainr, y_trainr), (X_testr, y_testr)],
+        verbose=100
+)
+
+future_dfr['prediction4'] = reg2.predict(future_dfr[FEATURES]).round()
+
+# Make chart 3 timeline predict death by region east
+
+dead_by_year_region4 = (
+        future_dfr['prediction4']
+)
+
+time_chart_line_r4 = pd.DataFrame(
+        dead_by_year_region4,
+)
+
+#West
+TARGETR = r_show5
+
+X_trainr = train[FEATURES]
+y_trainr = train[TARGETR]
+
+X_testr = test[FEATURES]
+y_testr = test[TARGETR]
+
+
+reg2 = xgb.XGBRegressor(**best_params)
+
+reg2.fit(X_trainr, y_trainr,
+        eval_set=[(X_trainr, y_trainr), (X_testr, y_testr)],
+        verbose=100
+)
+
+future_dfr['prediction5'] = reg2.predict(future_dfr[FEATURES]).round()
+
+# Make chart 3 timeline predict death by region west
+
+dead_by_year_region5 = (
+        future_dfr['prediction5']
+)
+
+time_chart_line_r5 = pd.DataFrame(
+        dead_by_year_region5,
+)
+
+#South
+TARGETR = r_show6
+
+X_trainr = train[FEATURES]
+y_trainr = train[TARGETR]
+
+X_testr = test[FEATURES]
+y_testr = test[TARGETR]
+
+
+reg2 = xgb.XGBRegressor(**best_params)
+
+reg2.fit(X_trainr, y_trainr,
+        eval_set=[(X_trainr, y_trainr), (X_testr, y_testr)],
+        verbose=100
+)
+
+future_dfr['prediction6'] = reg2.predict(future_dfr[FEATURES]).round()
+
+# Make chart 3 timeline predict death by region south
+
+dead_by_year_region6 = (
+        future_dfr['prediction6']
+)
+
+time_chart_line_r6 = pd.DataFrame(
+        dead_by_year_region6,
+)
+
+
+#______________________
+
+#plot chart 2 colums
+
+#left_column, right_column = st.columns(2)
+#left_column.line_chart(time_chart_line)
+#right_column.plotly_chart(fig_dead_month, use_container_width=True)
+
+
+
+st.markdown(f'##### Prediction of Death on Time Region : :red[{r_show}]')
 st.area_chart(time_chart_line_r)
+
+st.markdown(f'##### Prediction of Death on Time Region : :red[{r_show2}]')
+st.area_chart(time_chart_line_r2)
+
+st.markdown(f'##### Prediction of Death on Time Region : :red[{r_show3}]')
+st.area_chart(time_chart_line_r3)
+
+st.markdown(f'##### Prediction of Death on Time Region : :red[{r_show4}]')
+st.area_chart(time_chart_line_r4)
+
+st.markdown(f'##### Prediction of Death on Time Region : :red[{r_show5}]')
+st.area_chart(time_chart_line_r5)
+
+st.markdown(f'##### Prediction of Death on Time Region : :red[{r_show6}]')
+st.area_chart(time_chart_line_r6)
